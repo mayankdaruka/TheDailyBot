980eae65e7c2710aa6b399bae13ec46e2148aeea
Mayank Daruka <mayankdaruka2001@gmail.com>
2020-04-23 23:02:11 -0500

    Changes to main file

--- dailybot.py
+++ dailybot.py
@@ -1,4 +1,12 @@
 import slack
+from slackeventsapi import SlackEventAdapter
 import os
 
-client = slack.WebClient(os.environ['SLACK_BOT_TOKEN'], timeout=30)
+slack_bot_token = os.environ['SLACK_BOT_TOKEN']
+slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+client = slack.WebClient(slack_bot_token)
+slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
+
+@slack_events_adapter.on("message")
+def handle_message(event_data):
+    pass
