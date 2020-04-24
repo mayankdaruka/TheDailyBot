622f8f959ad7ad6b9fa5a7d31b6adf5daf0e1041
Mayank Daruka <mayankdaruka2001@gmail.com>
2020-04-24 10:57:22 -0500

    Added usage of RTM API for connection to bot

--- dailybot.py
+++ dailybot.py
@@ -5,22 +5,23 @@ import os
 slack_bot_token = os.environ['SLACK_BOT_TOKEN']
 slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
 client = slack.WebClient(slack_bot_token)
-slack_events_adapter = SlackEventAdapter(slack_signing_secret, endpoint="/slack/events")
+slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
 
-# To handle received message
-@slack_events_adapter.on("message")
-def handle_message(event_data):
-    message = event_data["event"]
-    # Check if message contains a subtype
-    # Examples of subtype: Member joined a channel, file was mentioned in channel, 
-    # group was renamed, message was deleted, group topic was updated etc.
-    if message["subtype"] is None:
-        if "test rankings" in message["text"]:
-            response = "Here are the test rankings: ***insert web scraped data***"
-            client.chat_postMessage(channel=message["channel"], text=response)
+# # To handle received message
+# @slack_events_adapter.on("message")
+# def handle_message(event_data):
+#     message = event_data["event"]
+#     # Check if message contains a subtype
+#     # Examples of subtype: Member joined a channel, file was mentioned in channel, 
+#     # group was renamed, message was deleted, group topic was updated etc.
+#     if message["subtype"] is None:
+#         if "test rankings" in message["text"]:
+#             response = "Here are the test rankings: ***insert web scraped data***"
+#             client.chat_postMessage(channel=message["channel"], text=response)
 
-@slack_events_adapter.on("error")
-def error_handler(error):
-    print("Error: " + str(error))
+# @slack_events_adapter.on("error")
+# def error_handler(error):
+#     print("Error: " + str(error))
+
 
 slack_events_adapter.start(port=3000)
---
+++ rtmbot.py
@@ -0,0 +1,27 @@
+import os
+from slack import RTMClient
+from slack.errors import SlackApiError
+
+
+print(os.environ)
+@RTMClient.run_on(event='message')
+def say_hello(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+    if 'text' in data and 'Hello' in data.get('text', []):
+        channel_id = data['channel']
+        user = data['user']
+        try:
+            response = web_client.chat_postMessage(
+                channel=channel_id,
+                text=f"Hi <@{user}>!",
+            )
+        except SlackApiError as e:
+            # You will get a SlackApiError if "ok" is False
+            assert e.response["ok"] is False
+            assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+            print(f"Got an error: {e.response['error']}")
+
+rtm_client = RTMClient(token=os.environ["CLASSIC_SLACK_BOT_TOKEN"])
+rtm_client.start()
